1583cba90893dee5b7884954e2f55c00bc6eb192
Sébastien Beuken <sebastien@sebisme.be>
2022-04-16 00:39:04 +0200

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,10 +6,13 @@ import 'rxjs/add/operator/map';
 
 import { Album } from './album';
 
+import { Product } from './product';
+
 @Injectable()
 export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
+  private _productsUrl: string = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -17,4 +20,7 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+  }
 }
